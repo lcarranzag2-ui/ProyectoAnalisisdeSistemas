97c6c42b30814201edd2d695ea09c23e00a256dd
CrischCode <cchamomorales@gmail.com>
2026-04-20 21:55:29 -0600

    feat: Estructura y configuracion inicial del proyecto

--- HiddenValley/HiddenValley.API/Program.cs
+++ HiddenValley/HiddenValley.API/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Configurar SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
