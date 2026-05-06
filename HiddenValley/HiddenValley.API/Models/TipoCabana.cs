8fe6071b728c1e36161b013fb48cb6510d2747f2
DeuceloR18 <daniloucelo087@gmail.com>
2026-05-06 00:47:26 -0600

    Implemntacion de edpoints crud

--- HiddenValley/HiddenValley.API/Models/TipoCabana.cs
+++ HiddenValley/HiddenValley.API/Models/TipoCabana.cs
@@ -3,6 +3,6 @@ namespace HiddenValley.API.Models;
 public class TipoCabana
 {
     public int Id { get; set; }
-    public string Nombre { get; set; }
+    public string Nombre { get; set;} = string.Empty; 
     public decimal Precio { get; set; }
 }
