97af940d75c41604ab7de3448ce2284f357e0527
Delmy <dfajardob@miumg.gt>
2026-05-16 14:14:02 -0600

    feat: API EstadoCabana
    creacion de enpoints de estado de cabaña

---
+++ HiddenValley/HiddenValley.Shared/DTOs/EstadoCabanaDto.cs
@@ -0,0 +1,8 @@
+namespace HiddenValley.Shared.DTOs;
+
+public class EstadoCabanaDto
+{
+    public int IdEstadoCabana { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string? Descripcion { get; set; }
+}
